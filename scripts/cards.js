1a325b02f13b84463430e2e9632226ee788a6849
Strateg <kotsiubaswyat@yandex.com>
2025-05-07 03:05:53 +0200

    maestro_5sprint

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,21 @@
-const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
-];
+const initialCards = [ 
+  { 
+    name: "Япония",
+    link: "https://images.unsplash.com/photo-1741851374655-3911c1b0e95a?w=500&auto=format&fit=crop&q=60", 
+  }, 
+  { name: "США", 
+    link: "https://images.unsplash.com/photo-1741367658528-8134fab3b67d?q=80&w=1974&auto=format&fit=crop", 
+  },
+  { name: "Исландия",
+    link: "https://images.unsplash.com/photo-1724376682600-6540c4570ac8?w=500&auto=format&fit=crop&q=60",
+   },
+  { name: "Шотландия",
+    link: "https://plus.unsplash.com/premium_photo-1732568404499-8561e0788a13?q=80&w=1974&auto=format&fit=crop", 
+  },
+  { name: "Перу", 
+    link: "https://images.unsplash.com/photo-1740004731264-3cde5c198cc2?q=80&w=1974&auto=format&fit=crop", 
+  }, 
+  { name: "Беларусь",
+    link: "https://images.unsplash.com/photo-1636590969448-bdfacae4a933?q=80&w=1935&auto=format&fit=crop", 
+  } 
+  ]; 
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,39 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+// Карточки
+const locationTemplate = document.querySelector("#card-template").content.querySelector(".card");
+const galleryContainer = document.querySelector(".places__list");
+
+// DOM-элемент 
+
+function generateLocationCard(info) {
+  const locationCard = locationTemplate.cloneNode(true);
+  const imageEl = locationCard.querySelector(".card__image");
+  const titleEl = locationCard.querySelector(".card__title");
+  const removeBtn = locationCard.querySelector(".card__delete-button");
+
+  imageEl.src = info.link;
+  imageEl.alt = info.name;
+  titleEl.textContent = info.name;
+
+  // Удаление 
+  removeBtn.addEventListener("click", () => {
+    locationCard.remove();
+  });
+
+  return locationCard;
+}
+
+// Отображение
+function displayGallery(items) {
+  items.forEach(item => {
+    const cardElement = generateLocationCard(item);
+    galleryContainer.appendChild(cardElement);
+  });
+}
+
+// Запуск
+displayGallery(initialCards);
