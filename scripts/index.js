8b02cce4f0ea6048c118e4fd8b8153ff6d692735
Strateg <kotsiubaswyat@yandex.com>
2025-05-07 15:49:21 +0200

    fix_callback

--- scripts/index.js
+++ scripts/index.js
@@ -9,37 +9,41 @@
 // @todo: Вывести карточки на страницу
 
 
-// Карточки
+
 const locationTemplate = document.querySelector("#card-template").content.querySelector(".card");
 const galleryContainer = document.querySelector(".places__list");
 
-// DOM-элемент 
+//  удаление 
+function DeleteCard(cardElement) {
+  cardElement.remove();
+}
 
-function generateLocationCard(info) {
+//  создание
+function createCard(data, onDelete) {
   const locationCard = locationTemplate.cloneNode(true);
   const imageEl = locationCard.querySelector(".card__image");
   const titleEl = locationCard.querySelector(".card__title");
   const removeBtn = locationCard.querySelector(".card__delete-button");
 
-  imageEl.src = info.link;
-  imageEl.alt = info.name;
-  titleEl.textContent = info.name;
+  imageEl.src = data.link;
+  imageEl.alt = data.name;
+  titleEl.textContent = data.name;
 
-  // Удаление 
+  //  колбэк
   removeBtn.addEventListener("click", () => {
-    locationCard.remove();
+    onDelete(locationCard);
   });
 
   return locationCard;
 }
 
-// Отображение
-function displayGallery(items) {
-  items.forEach(item => {
-    const cardElement = generateLocationCard(item);
-    galleryContainer.appendChild(cardElement);
-  });
+//  DOM
+function renderCard(cardElement) {
+  galleryContainer.appendChild(cardElement);
 }
 
-// Запуск
-displayGallery(initialCards);
+// Отображение
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, DeleteCard);
+  renderCard(card);
+});
