90472fe9553147ef8022fbdf9949b5abde7c3e6f
Strateg <kotsiubaswyat@yandex.com>
2025-06-07 14:36:41 +0200

    maestro_Taras

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,70 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-
-
 const locationTemplate = document.querySelector("#card-template").content.querySelector(".card");
 const galleryContainer = document.querySelector(".places__list");
 
-//  удаление 
-function DeleteCard(cardElement) {
+// Удаление
+function deleteCard(cardElement) {
   cardElement.remove();
 }
 
-//  создание
+// Создание карточки
 function createCard(data, onDelete) {
   const locationCard = locationTemplate.cloneNode(true);
   const imageEl = locationCard.querySelector(".card__image");
   const titleEl = locationCard.querySelector(".card__title");
   const removeBtn = locationCard.querySelector(".card__delete-button");
+  const likeBtn = locationCard.querySelector(".card__like-button");
 
   imageEl.src = data.link;
   imageEl.alt = data.name;
   titleEl.textContent = data.name;
 
-  //  колбэк
   removeBtn.addEventListener("click", () => {
     onDelete(locationCard);
+  });
+
+  likeBtn.addEventListener("click", () => {
+    likeBtn.classList.toggle("card__like-button_is-active");
   });
 
   return locationCard;
 }
 
-//  DOM
+// Добавление карточки в DOM
 function renderCard(cardElement) {
   galleryContainer.appendChild(cardElement);
 }
 
-// Отображение
+// Отображение всех карточек
 initialCards.forEach((cardData) => {
-  const card = createCard(cardData, DeleteCard);
+  const card = createCard(cardData, deleteCard);
   renderCard(card);
+});
+
+
+// POP
+
+
+const popupEditProfile = document.querySelector(".popup_type_edit");
+
+const popupAddCard = document.querySelector(".popup_type_new-card");
+const popupImage = document.querySelector(".popup_type_image");
+
+const btnEditProfile = document.querySelector(".profile__edit-button");
+const btnAddCard = document.querySelector(".profile__add-button");
+
+
+
+// функция открытия поп
+function showModal(popupElement) {
+  popupElement.classList.add("popup_opened");
+  document.addEventListener("keydown", handleEscClose);
+}
+
+
+
+// кнопка открытия
+btnEditProfile.addEventListener("click", () => {
+  inputName.value = profileTitle.textContent;
+  inputJob.value = profileSubtitle.textContent;
+  showModal(popupEditProfile);
 });
